bdc44b5ff8a2a561ebfabc86d3c789eee33420ff
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-05-07 16:53:12 -0600

    Made routes

---
+++ config/routes.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var userController = require('../controllers/users.js');
+var sessionController = require('../controllers/sessions.js');
+
+
+//User Routes
+
+//index
+router.get('/api/users', usersController.index);
+
+//Create
+router.post('/api/users', usersController.create);
+
+//show
+router.get('/api/users/:id', usersController.show);
+
+//update
+router.put('/api/users/:id', usersController.update);
+
+//destroy
+router.delete('/api/users/:id', usersController.destroy);
+
+//Session Routes
+
+//index
+router.get('/api/sessions', sessionsController.index);
+
+//Create
+router.post('/api/sessions', sessionsController.create);
+
+//Show
+router.get('/api/sessions/:id', sessionsController.show);
+
+//Update
+router.put('/api/sessions/:id', sessionsController.update);
+
+//Destory
+router.delete('/api/sessions/:id', sessionsController.destroy);
+
+module.exports = router;
