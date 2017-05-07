f2957f33b8bdbff8b093322eb87f9c157c0b7049
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-05-07 16:38:35 -0600

    Created db.Setup

--- db/dbSetup.js
+++ db/dbSetup.js
@@ -1,5 +1,5 @@
-var DB = require('../models');
+var DB = require("../models");
 
-DB.sequelize.sync{{force: true}}.then(function() {
+DB.sequelize.sync({force: true}).then(function(){
   process.exit();
 });
