f644e172f5ce4aa2942794f3ea4a0534941d52f5
Taylor Laine <tlaine@msudenver.edu>
2017-05-08 11:34:01 -0600

    Cleaned up code and new key

--- db/dbSetup.js
+++ db/dbSetup.js
@@ -1,5 +1,7 @@
+// Require models to connect to database
 var DB = require("../models");
 
+// Set up tables and clear any data in them
 DB.sequelize.sync({force: true}).then(function(){
   process.exit();
 });
